233adb1801ccea011b581f236efe5ce00d9ce5d6
Kamal <ks708570@gmail.com>
2024-01-27 01:16:11 +0530

    Added: vercel speed insight feat

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import { Toaster } from 'react-hot-toast';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+
 const Layout = ({ children }) => {
+
   return (
     <>
       <Header />
-      <main>
+      <main style={{ minHeight: '70vh' }}>
         <Toaster />
+        <SpeedInsights />
         {children}
       </main>
       <Footer />
